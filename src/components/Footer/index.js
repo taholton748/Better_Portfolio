34a520d9a05939df61197be6ddac3e51fd630bb6
Medici <107539009+taholton748@users.noreply.github.com>
2023-01-13 06:33:56 -0700

    formatted footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./index.css";
 
 function Footer() {
   return (
-    <footer class="footer">
-    <a href="https://github.com/taholton748">GitHub</a>
-    <a href="https://www.linkedin.com/in/tyler-holton-24423690/">LinkedIn</a>
-    <a href="https://www.instagram.com/t_holt88/?hl=bn">Instagram</a>
+    <footer className="footer">
+    <a className="footerLink" href="https://github.com/taholton748">GitHub</a>
+    <a className="footerLink" href="https://www.linkedin.com/in/tyler-holton-24423690/">LinkedIn</a>
+    <a className="footerLink" href="https://www.instagram.com/t_holt88/?hl=bn">Instagram</a>
   </footer>
   );
 }
